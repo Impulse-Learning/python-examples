8cfee1b62a90fbf1d615c5d08571fb8191aadbe6
rdoughty10 <rdoughty20@sjs.org>
2020-06-02 17:34:35 -0500

    first projects

---
+++ firstProgram.py
@@ -0,0 +1,21 @@
+print("Hello World")
+
+word = "Hello World"
+print(word)
+
+name = input ("What is your name")
+print("Hello ", name)
+
+num1 = input("Give me a number")
+num2 = input("Give me a number")
+# do this first without the ints
+sum = int(num1) + int(num2)
+
+
+boolean = True
+integr = 12
+string = "Hello"
+dbl = 12.12
+char = 'a'
+
+
---
+++ receipt.py
@@ -0,0 +1,6 @@
+appetizer = int(input("How much did your appetizer cost?"))
+entree = int(input("How much did your entree cost?"))
+desert = int(input("How much did your desert cost?"))
+
+cost = appetizer + entree + desert
+print("The cost of your meal was", cost, "$")
